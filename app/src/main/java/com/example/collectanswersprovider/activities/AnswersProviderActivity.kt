1daa099ebcb4de5090cfc504edef7df2eb6535c0
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2021-02-01 23:33:08 +0100

    Code improvements

--- app/src/main/java/com/example/collectanswersprovider/activities/AnswersProviderActivity.kt
+++ app/src/main/java/com/example/collectanswersprovider/activities/AnswersProviderActivity.kt
@@ -3,6 +3,7 @@ package com.example.collectanswersprovider.activities
 import android.app.Activity
 import android.content.ClipData
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -28,9 +29,7 @@ class AnswersProviderActivity : AppCompatActivity() {
     }
 
     private fun returnAnswer() {
-        val intent = Intent(Intent.ACTION_SEND_MULTIPLE).apply {
-            clipData = ClipData.newRawUri(null, null)
-        }
+        val intent = Intent(Intent.ACTION_SEND_MULTIPLE)
 
         for (questionToPopulate in viewModel.questionsToPopulate) {
             val key = viewModel.getExtraKey(questionToPopulate)
@@ -38,30 +37,23 @@ class AnswersProviderActivity : AppCompatActivity() {
                 questionToPopulate.contains(INTEGER, true) -> intent.putExtra(key, viewModel.getIntegerAnswer())
                 questionToPopulate.contains(DECIMAL, true) -> intent.putExtra(key, viewModel.getDecimalAnswer())
                 questionToPopulate.contains(TEXT, true) -> intent.putExtra(key, viewModel.getTextAnswer())
-                questionToPopulate.contains(IMAGE, true) -> {
-                    val uri = viewModel.getImageAnswer();
-                    intent.putExtra(key, uri)
-                    intent.clipData?.addItem(ClipData.Item(uri))
-                }
-                questionToPopulate.contains(AUDIO, true) -> {
-                    val uri = viewModel.getAudioAnswer();
-                    intent.putExtra(key, uri)
-                    intent.clipData?.addItem(ClipData.Item(uri))
-                }
-                questionToPopulate.contains(VIDEO, true) -> {
-                    val uri = viewModel.getVideoAnswer();
-                    intent.putExtra(key, uri)
-                    intent.clipData?.addItem(ClipData.Item(uri))
-                }
-                questionToPopulate.contains(FILE, true) -> {
-                    val uri = viewModel.getFileAnswer();
-                    intent.putExtra(key, uri)
-                    intent.clipData?.addItem(ClipData.Item(uri))
-                }
+                questionToPopulate.contains(IMAGE, true) -> addItem(intent, key, viewModel.getImageAnswer())
+                questionToPopulate.contains(AUDIO, true) -> addItem(intent, key, viewModel.getAudioAnswer())
+                questionToPopulate.contains(VIDEO, true) -> addItem(intent, key, viewModel.getVideoAnswer())
+                questionToPopulate.contains(FILE, true) -> addItem(intent, key, viewModel.getFileAnswer())
             }
         }
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         setResult(Activity.RESULT_OK, intent)
         finish()
+    }
+
+    private fun addItem(intent: Intent, key: String, uri: Uri) {
+        intent.putExtra(key, uri)
+        if (intent.clipData == null) {
+            intent.clipData = ClipData.newRawUri(null, uri)
+        } else {
+            intent.clipData?.addItem(ClipData.Item(uri))
+        }
     }
 }
